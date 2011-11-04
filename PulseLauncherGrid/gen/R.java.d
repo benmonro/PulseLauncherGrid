cf6e819b81d775050ef961bb20caf03cf71dcdd7
ben monro <ben.monro@neudesic.com>
2011-11-04 14:18:32 -0700

    build.xml

---
+++ PulseLauncherGrid/gen/R.java.d
@@ -0,0 +1,11 @@
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\gen\com\neudesic\mobile\pulse\ui\launcher\R.java \
+ : C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\res\drawable-hdpi\avatar.png \
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\res\drawable-hdpi\ic_launcher.png \
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\res\drawable-ldpi\avatar.png \
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\res\drawable-ldpi\ic_launcher.png \
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\res\drawable-mdpi\avatar.png \
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\res\drawable-mdpi\ic_launcher.png \
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\res\layout\grid_view_item_layout.xml \
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\res\layout\main.xml \
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\res\values\strings.xml \
+C:\Users\ben.monro\workspace\PulseLauncherGrid\PulseLauncherGrid\AndroidManifest.xml \
